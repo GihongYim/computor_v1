397afedb24c50cfa7d2c83f0c1c0be348c054dcb
Gihong Yim <gyim@c2r8s1.42seoul.kr>
2024-05-10 21:17:22 +0900

    asdf

--- computor.py
+++ computor.py
@@ -1,11 +1,40 @@
 import sys
 
+def is_float(f : str):
+    try:
+        float(f)
+        return True
+    except ValueError:
+        return False
 
-def polynomial(p : str):
-    poly = dict
-    term = p.split("+")
-    print(term)
-    pass
+
+def polynomial(p : str) -> list:
+    poly = {}
+    sign = 1.0
+    coef = 1.0
+    x_pow = 0
+    
+    for word in p.split():
+        if word == '+' or word == '-':
+            if x_pow not in poly.keys():
+                poly[x_pow] = 0.0
+            poly[x_pow] += sign * coef
+            if word == '+':
+                coef = 1.0
+            elif word == '-':
+                coef = -1.0
+            x_pow = 0
+        elif is_float(word):
+            coef *= float(word)
+        elif word[0:2] == "X^" and is_float(word[2:]):
+            x_pow += int(word[2:])
+    if x_pow not in poly.keys():
+        poly[x_pow] = 0.0
+    poly[x_pow] = coef
+    print(poly)
+    return poly
+        
+    
 
 def computor(equation: str):
     try:
@@ -20,11 +49,16 @@ def computor(equation: str):
 
     left = left_right[0]
     right = left_right[1]
-    print(left)
-    print(right)
 
     left_poly = polynomial(left)
     right_poly = polynomial(right)
+
+    for x_pow, coef in right_poly.items():
+        if x_pow not in left_poly.keys():
+            left_poly[x_pow] = 0.0
+        left_poly[x_pow] -= coef
+    
+    print(left_poly)
 
 
 
